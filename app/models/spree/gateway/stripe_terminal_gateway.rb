e45a1cf82fa4e8909d329082974cc8c2c708bbc9
Nitin Kabi <nitin.kabi@hyscaler.com>
2024-08-02 13:06:05 +0530

    first commit

---
+++ app/models/spree/gateway/stripe_terminal_gateway.rb
@@ -0,0 +1,53 @@
+module Spree
+  class Gateway::StripeTerminalGateway < Spree::Gateway::StripeGateway
+    def method_type
+      'stripe_terminal'
+    end
+
+    def provider_class
+      ActiveMerchant::Billing::StripeTerminalGateway
+    end
+
+    def payment_source_class
+      PhysicalCreditCard
+    end
+
+    def supports?(source)
+      source.is_a? payment_source_class
+    end
+
+    def purchase(_money, _creditcard, _gateway_options)
+      raise NotImplementedError
+    end
+
+    def show(response_code)
+      provider.show_intent(response_code, {})
+    end
+
+    def receipt(response_code)
+      payment_intent = show(response_code)
+      charges = payment_intent.params['charges']['data']
+      charges&.last['payment_method_details']['card_present']['receipt']
+    end
+
+    def create_profile(_payment)
+      return
+    end
+
+    def cancel(response_code)
+      payment = Spree::Payment.valid.where(
+        response_code: response_code,
+        source_type:   payment_source_class.to_s
+      ).first
+
+      return if payment.nil?
+
+      if payment.pending?
+        payment.void_transaction!
+      elsif payment.completed? && payment.can_credit?
+        provider.refund(payment.credit_allowed.to_money.cents, response_code)
+      end
+    end
+
+  end
+end
